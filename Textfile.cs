49a89bd82775612c9cb1acfe8d81a2beb08bbe61
LungeloZungu <20103170@vcconnect.co.za>
2021-05-03 17:48:12 +0200

    Add project files.

---
+++ Textfile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO; 
+
+namespace ToDoListApp
+{
+    class Textfile
+    {
+
+        public static  void WriteToFile()
+        {
+
+            try
+            {
+                using (StreamWriter write = new StreamWriter("List.txt"))
+                {
+                    foreach (string line in ToDoListList.Task)
+                    {
+                        write.WriteLine(line);
+
+                       
+                        write.Close();
+                    }
+                }       
+                
+            }
+               catch  (Exception ex)
+                {
+                Console.WriteLine(ex.Message);
+                }
+
+        }
+    }
+}
---
+++ ToDoListList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoListApp
+{
+    class ToDoListList
+    {
+
+      public static  List<string > Task = new List<string>();
+    }
+}
